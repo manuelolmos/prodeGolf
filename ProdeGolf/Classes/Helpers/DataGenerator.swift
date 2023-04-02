37628743e05a3dced599083b5b9449e46e6d6451
Manuel Olmos Gil <manuel.olmos@airtime.com>
2023-04-02 16:15:59 -0300

    Add missing player

--- ProdeGolf/Classes/Helpers/DataGenerator.swift
+++ ProdeGolf/Classes/Helpers/DataGenerator.swift
@@ -14,7 +14,7 @@ struct DataGenerator {
         ["Collin Morikawa", "Tony Finau", "Cameron Young", "Matt Fitzpatrick", "Jordan Spieth", "Tyrrell Hatton", "Sungjae Im", "Tom Kim", "Kurt Kitayama", "Hideki Matsuyama"],
         ["Keegan Bradley", "Shane Lowry", "Billy Horschel", "Tom Hoge", "Tommy Fleetwood", "Brian Harman", "Sahith Theegala", "Justin Rose", "Sepp Straka", "Jason Day", "Chris Kirk", "Seamus Power", "Ryan Fox", "Russell Henley", "Adam Scott", "Si Woo Kim", "Corey Conners", "Kyoung-Hoon Lee", "Alexander Noren", "Harris English"],
         ["Keith Mitchell", "Min Woo Lee", "Kevin Kisner", "J.T. Poston", "Taylor Moore", "Mackenzie Hughes", "Adam Svensson", "Adrian Meronk", "Harold Varner", "Scott Stallings", "Kazuki Higa", "Danny Willett", "Gary Woodland", "Francesco Molinari", "Cameron Champ", "Tiger Woods", "Sam Bennett", "Ben Carr", "Zach Johnson", "Aldrich Potgieter", "Harrison Crowe", "Matthew McClean", "Bernhard Langer", "Fred Couples", "Gordon Sargent", "Jose Maria Olazabal", "Larry Mize", "Mateo Fernandez de Oliveira", "Mike Weir", "Sandy Lyle", "Vijay Singh"],
-        ["Cameron Smith", "Joaquin Niemann", "Abraham Ancer", "Thomas Pieters", "Talor Gooch", "Dustin Johnson", "Patrick Reed", "Jason Kokrak", "Kevin Na", "Brooks Koepka", "Louis Oosthuizen", "Bryson DeChambeau", "Sergio Garcia", "Bubba Watson", "Charl Schwartzel", "Phil Mickelson"]
+        ["Cameron Smith", "Joaquin Niemann", "Abraham Ancer", "Thomas Pieters", "Talor Gooch", "Mito Pereira", "Dustin Johnson", "Patrick Reed", "Jason Kokrak", "Kevin Na", "Brooks Koepka", "Louis Oosthuizen", "Bryson DeChambeau", "Sergio Garcia", "Bubba Watson", "Charl Schwartzel", "Phil Mickelson"]
     ]
 
     func generateGamblers() -> [Gambler] {
