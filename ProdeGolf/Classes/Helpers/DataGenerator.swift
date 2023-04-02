9cf44c58ab9fdae114b00f62376ea534be2ce735
Manuel Olmos Gil <manuel.olmos@airtime.com>
2023-04-02 15:18:38 -0300

    Update ranking

--- ProdeGolf/Classes/Helpers/DataGenerator.swift
+++ ProdeGolf/Classes/Helpers/DataGenerator.swift
@@ -10,10 +10,11 @@ import Foundation
 struct DataGenerator {
 
     private let ranking = [
-        ["Rory", "Scottie", "Rahm", "Morikawa", "Hovland"],
-        ["Homa", "Finau", "Burns", "Day", "Rose"],
-        ["Thomas", "Cantlay", "Xander", "Fowler", "Young"],
-        ["Speith", "T. Kim", "S.W Kim", "Kuchar", "Woods"]
+        ["Scottie Scheffler", "Rory McIlroy", "Jon Rahm", "Patrick Cantlay", "Max Homa", "Xander Schauffele", "Will Zalatoris", "Viktor Hovland", "Sam Burns", "Justin Thomas"],
+        ["Collin Morikawa", "Tony Finau", "Cameron Young", "Matt Fitzpatrick", "Jordan Spieth", "Tyrrell Hatton", "Sungjae Im", "Tom Kim", "Kurt Kitayama", "Hideki Matsuyama"],
+        ["Keegan Bradley", "Shane Lowry", "Billy Horschel", "Tom Hoge", "Tommy Fleetwood", "Brian Harman", "Sahith Theegala", "Justin Rose", "Sepp Straka", "Jason Day", "Chris Kirk", "Seamus Power", "Ryan Fox", "Russell Henley", "Adam Scott", "Si Woo Kim", "Corey Conners", "Kyoung-Hoon Lee", "Alexander Noren", "Harris English"],
+        ["Keith Mitchell", "Min Woo Lee", "Kevin Kisner", "J.T. Poston", "Taylor Moore", "Mackenzie Hughes", "Adam Svensson", "Adrian Meronk", "Harold Varner", "Scott Stallings", "Kazuki Higa", "Danny Willett", "Gary Woodland", "Francesco Molinari", "Cameron Champ", "Tiger Woods", "Sam Bennett", "Ben Carr", "Zach Johnson", "Aldrich Potgieter", "Harrison Crowe", "Matthew McClean", "Bernhard Langer", "Fred Couples", "Gordon Sargent", "Jose Maria Olazabal", "Larry Mize", "Mateo Fernandez de Oliveira", "Mike Weir", "Sandy Lyle", "Vijay Singh"],
+        ["Cameron Smith", "Joaquin Niemann", "Abraham Ancer", "Thomas Pieters", "Talor Gooch", "Dustin Johnson", "Patrick Reed", "Jason Kokrak", "Kevin Na", "Brooks Koepka", "Louis Oosthuizen", "Bryson DeChambeau", "Sergio Garcia", "Bubba Watson", "Charl Schwartzel", "Phil Mickelson"]
     ]
 
     func generateGamblers() -> [Gambler] {
