619c88cd9e192baa68b057d839bd83d0f6b1cafe
Manuel Olmos Gil <manuel.olmos@airtime.com>
2023-04-02 22:18:11 -0300

    Fix bucket number on gambler details

--- ProdeGolf/Classes/ViewControllers/DetailGamblerViewController.swift
+++ ProdeGolf/Classes/ViewControllers/DetailGamblerViewController.swift
@@ -18,7 +18,7 @@ class DetailGamblerViewController: UIViewController {
         if let gambler = self.gambler {
             var wishPicksDescription = ""
             for (index, value) in gambler.wishPicks.enumerated() {
-                wishPicksDescription.append("Bucket \(index) - \(value.joined(separator: ", "))\n")
+                wishPicksDescription.append("Bucket \(index+1) - \(value.joined(separator: ", "))\n")
             }
             gamblerLabel.text = "Name: \(gambler.name) \nPicks: \(gambler.picks.joined(separator: ", "))\nDraft positions: \(gambler.draftPositions)\nWish picks:\n\(wishPicksDescription)"
         }
